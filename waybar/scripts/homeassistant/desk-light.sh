aeabcd9de662cc69d3968a5b8188b8d47ffc85f8
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2023-06-08 18:20:00 +0200

    Turn off light on lock screen

--- waybar/scripts/homeassistant/desk-light.sh
+++ waybar/scripts/homeassistant/desk-light.sh
@@ -2,8 +2,36 @@
 
 source /home/choffmann/.config/HyprScape/secrets.conf
 
-curl \
+toggle() {
+  curl \
     -H "Authorization: Bearer $HOMEASSISTANT_TOKEN" \
     -H "Content-Type: application/json" \
     -d '{"entity_id": "light.schreibtisch"}' \
     "https://cloud.jgdperl.com/api/services/light/toggle"
+}
+
+turn_on() {
+  curl \
+    -H "Authorization: Bearer $HOMEASSISTANT_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d '{"entity_id": "light.schreibtisch"}' \
+    "https://cloud.jgdperl.com/api/services/light/turn_on"
+}
+
+turn_off() {
+  curl \
+    -H "Authorization: Bearer $HOMEASSISTANT_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d '{"entity_id": "light.schreibtisch"}' \
+    "https://cloud.jgdperl.com/api/services/light/turn_off"
+}
+
+if [[ $1 == "--toggle" ]]; then
+  toggle
+elif [[ $1 == "--turn-on" ]]; then
+  turn_on
+elif [[ $1 == "--turn-off" ]]; then
+  turn_off
+else
+  toggle
+fi
